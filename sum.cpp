ca2f2f53b296cefe8fdc2ff9a8ed440e3418f669
Bhargav Nandan Chaturvedi <bhargavchaturvedi108@gmail.com>
2023-01-12 23:55:40 +0530

    added a message and a new line in sum.cpp

---
+++ sum.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b,c;
+    cout<<"the numbers are "<<endl;
+    cin>>a>>b;
+    c=a+b;
+    cout<<"sum is "<<c<<"hsdhfkashf"<<endl;
+    cout<<"make a change";
+
+
+return 0;
+}
