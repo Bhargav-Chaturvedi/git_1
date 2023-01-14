9c027697b252d7f16824f987cf8e94236da00e0a
Bhargav Nandan Chaturvedi <bhargavchaturvedi108@gmail.com>
2023-01-15 00:38:07 +0530

    added multi.cpp

--- sum.cpp
+++ sum.cpp
@@ -4,10 +4,7 @@ int main(){
     int a,b,c;
     cout<<"the numbers are "<<endl;
     cin>>a>>b;
-    c=a+b;
-    cout<<"sum is "<<c<<"hsdhfkashf"<<endl;
-    cout<<"make a change";
-
-
+    c=a*b;
+    cout<<"multiplication is "<<c<<endl;
 return 0;
 }
